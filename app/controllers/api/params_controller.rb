3e1743b5b25af7ed48971ff15e275f3f9a5a5bda
natalie <nataliediazrodriguez@gmail.com>
2019-07-06 16:17:50 -0400

    adding first letter a query

--- app/controllers/api/params_controller.rb
+++ app/controllers/api/params_controller.rb
@@ -1,7 +1,12 @@
 class Api::ParamsController < ApplicationController
   def query 
-  p 'params'
-  p  params["name"]
+ name = params["name"]
+  first_character = params["name"][0]
+  if first_character == "a"
+    @message = "the first letter of your name is A"
+else 
+ @message = "your name is #{name}"
+end
   render "query_paramater.json.jb"
   end
 end
