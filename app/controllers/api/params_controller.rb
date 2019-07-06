2e758d0fea2f6ea288bb5083c367532c58337ccf
natalie <nataliediazrodriguez@gmail.com>
2019-07-06 19:21:45 -0400

    create a  url segment

--- app/controllers/api/params_controller.rb
+++ app/controllers/api/params_controller.rb
@@ -9,4 +9,22 @@ else
 end
   render "query_paramater.json.jb"
   end
+
+  def game 
+  #get user input 
+  the_guess= params['user_guess'].to_i
+  #compare input to actual answer 
+  if the_guess == 32
+    p ' good guess '
+    elsif the_guess > 32
+    p 'too high, try again'
+  elsif the_guess < 32 
+    p 'too low , try again'
+  end
+  #give the user message based on their guess 
+  render "guess.json.jb"
 end
+  def url_segment_method
+    render "segment_view.json.jb"
+  end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
-    get "/query_url" => "params#query"
-  #   get "/photos" => "photos#index"
+    get '/query_url' => 'params#query'
+    get '/guessing_game' => 'params#game'
+    get '/segment_parameter_url/:this_is_a_variable' => 'params#url_segment_method'
   end
 end
